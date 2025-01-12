d5689da35eb578db0a1641d2b9a2b705a0191b27
zynxzowil3 <xdyutu282@gmail.com>
2025-01-12 02:29:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7825259011:AAEd0ws08eDNebgKw0PWQhjzWsUW9Kj9xmA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("zynxzowil")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6656216693")
 
 
 
